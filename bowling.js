83692e68505f88c4debac6b74a0fdbcd102981ff
Ivan <podgur5@gmail.com>
2023-04-17 21:41:32 +0100

    - increment score for each roll - validate correct number of pins rolled

--- bowling.js
+++ bowling.js
@@ -1,14 +1,20 @@
+const MAX_NUMBER_OF_PINS = 10;
+
+const PINS_NUMBER_OUT_OF_BOUNDARIES_ERROR = `Number of pins must between 0 and ${MAX_NUMBER_OF_PINS}`;
+
 class Bowling {
   constructor() {
-
+    this.score = 0;
   }
 
   roll(pins) {
-
+    if (pins < 0 || pins > MAX_NUMBER_OF_PINS)
+      throw new Error(PINS_NUMBER_OUT_OF_BOUNDARIES_ERROR);
+    this.score += pins;
   }
 
   getScore() {
-
+    return this.score;
   }
 }
 
--- bowling.test.js
+++ bowling.test.js
@@ -1,9 +1,12 @@
-const Bowling = require('./bowling');
+const Bowling = require("./bowling");
 
-describe('Bowling', () => {
-  it('handles a very boring game', () => {
-    const bowling = new Bowling();
+describe("Bowling", () => {
+  let bowling;
+  beforeEach(() => {
+    bowling = new Bowling();
+  });
 
+  it("handles a very boring game", () => {
     bowling.roll(0);
     bowling.roll(0);
     bowling.roll(0);
@@ -26,5 +29,25 @@ describe('Bowling', () => {
     bowling.roll(0);
 
     expect(bowling.getScore()).toBe(0);
+  });
+
+  it("increments total score with each roll", () => {
+    bowling.roll(1);
+    bowling.roll(1);
+    bowling.roll(1);
+    bowling.roll(2);
+    expect(bowling.getScore()).toBe(5);
+  });
+
+  it("throws an error if less than 0 pins rolled", () => {
+    expect(() => {
+      bowling.roll(-1);
+    }).toThrow(bowling.PINS_NUMBER_OUT_OF_BOUNDARIES_ERROR);
+  });
+
+  it("throws an error if more than 10 pins rolled", () => {
+    expect(() => {
+      bowling.roll(22);
+    }).toThrow(bowling.PINS_NUMBER_OUT_OF_BOUNDARIES_ERROR);
   });
 });

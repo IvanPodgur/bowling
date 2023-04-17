45771230b616eefbe2d4386e7bea3b75473f5431
Ivan <podgur5@gmail.com>
2023-04-17 21:52:28 +0100

    -keep array of frames in the game - each frame is an object - keep track of the current frame

--- bowling.js
+++ bowling.js
@@ -5,16 +5,30 @@ const PINS_NUMBER_OUT_OF_BOUNDARIES_ERROR = `Number of pins must between 0 and $
 class Bowling {
   constructor() {
     this.score = 0;
+    this.frames = [];
+    this.currentFrame = {
+      rolls: [],
+      total: 0
+    };
   }
 
   roll(pins) {
     if (pins < 0 || pins > MAX_NUMBER_OF_PINS)
       throw new Error(PINS_NUMBER_OUT_OF_BOUNDARIES_ERROR);
     this.score += pins;
+    this.currentFrameIndex++;
   }
 
   getScore() {
     return this.score;
+  }
+
+  getCurrentFrameIndex() {
+    return this.frames.length;
+  }
+
+  getCurrentFrame() {
+    return this.currentFrame;
   }
 }
 
--- bowling.test.js
+++ bowling.test.js
@@ -50,4 +50,10 @@ describe("Bowling", () => {
       bowling.roll(22);
     }).toThrow(bowling.PINS_NUMBER_OUT_OF_BOUNDARIES_ERROR);
   });
+
+  it("begins game with a clean frame with index 0", () => {
+    expect(bowling.getCurrentFrameIndex()).toBe(0);
+    expect(bowling.getCurrentFrame().rolls.length).toBe(0);
+    expect(bowling.getCurrentFrame().total).toBe(0);
+  });
 });
